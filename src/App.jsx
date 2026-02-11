e7585488a93dc964c87c676af26dacadf9f5d3d6
vineettiwari1708 <vineettiwari1708@email.com>
2026-02-11 18:17:29 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div>
-      hi this is vineet
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/profile' element={<ProfilePage/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = ({selectedUser, setSelectedUser}) => {
+  return (
+    <div>
+      <h1>sidebar</h1>
+    </div>
+  )
+}
+
+export default Sidebar
